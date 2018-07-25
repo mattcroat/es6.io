cdfb74d443a8d0d641b14a94d70020281b7c94ee
mattcroat <mattcroat.dev@gmail.com>
2018-07-26 00:40:40 +0200

    Initial commit

--- 14 - ES6 Tooling/systemjs/main.js
+++ 14 - ES6 Tooling/systemjs/main.js
@@ -1,7 +1,7 @@
+// go to npm's registry and import lodash
 import { sum, kebabCase } from 'npm:lodash';
 import { addTax } from './checkout';
 
 console.log(kebabCase('Wes is soooo cool ⛓⛓⛓⛓'));
-
 
 console.log(addTax(100, 0.15));
